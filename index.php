47712dd5d49641d767a9a7a73b5cdc4f98c4eada
auricio52 <auricio52@gmail.com>
2021-06-26 14:21:17 -0300

    Finalizado landing page

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Projeto 06</title>
+
+        <meta charset="utf-8">
+        <meta name="keywords" content="Palavras, chaves, separadas, por, virgula">
+        <meta name="author" content="Aurício">
+        <meta name="description" content="Descrição do meu site">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+        <link rel="stylesheet" href="css/font-awesome.min.css" />
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/styles.css" />
+    </head>
+    <body>
+       
+        <section class="main">
+            <div class="layer"></div>
+            <div class="center">
+                <header>
+                    <div class="icons-social">
+                        <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    </div>
+
+                    <div class="clear"></div>
+
+                    <div class="logo">Danki code</div>
+
+                    <nav class="desktop">
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Sobre</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                    </nav>
+
+                    <nav class="mobile">
+                        <i class="fa fa-bars"></i>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Sobre</a></li>
+                            <li><a href="">Contato</a></li>
+                        </ul>
+                    </nav>
+
+                    <div class="clear"></div>
+                </header>
+
+                <div class="conteudo-header">
+                    <h2>Bem-vindo(a) ao nosso <span>Website</span></h2>
+
+                    <p>Sed id erat ultrices, rutrum enim scelerisque, euismod nunc. Praesent et tempor eros. 
+                        Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                         Pellentesque eleifend odio ut efficitur facilisis. Quisque lacus lectus, elementum ut pellentesque quis, 
+                         semper eget enim. Nam suscipit tempus sapien. Sed nec justo massa.</p>
+
+                    <p>Sed id erat ultrices, rutrum enim scelerisque, euismod nunc. Praesent et tempor eros. 
+                    Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                    Pellentesque eleifend odio ut efficitur facilisis. </p>
+
+                    <a href="">Saiba mais</a>
+                </div>
+            </div>
+        </section>
+
+        <section class="planos">
+            <div class="chamada-planos">
+                <div class="center">
+                    <h2>Confira nossos planos</h2>
+                </div>
+            </div>
+
+            <section class="box-planos">
+                <div class="center">
+                    <div class="box-planos-single">
+                       <div class="box-planos-wrapper">
+                            <h2>Título da oferta</h2>
+
+                            <div class="pontos-plano">
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            </div>
+
+                            <div class="preco">
+                                <h2>R$ 250,00</h2>
+                            </div>
+                       </div>
+                    </div>
+
+                    <div class="box-planos-single">
+                        <div class="box-planos-wrapper">
+                            <h2>Título da oferta</h2>
+
+                            <div class="pontos-plano">
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            </div>
+
+                            <div class="preco">
+                                <h2>R$ 250,00</h2>
+                            </div>
+                       </div>
+                    </div>
+
+                    <div class="box-planos-single">
+                        <div class="box-planos-wrapper">
+                            <h2>Título da oferta</h2>
+
+                            <div class="pontos-plano">
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                                <p><i class="fa fa-check-square"></i>Lorem ipsum silor domor</p>
+                            </div>
+
+                            <div class="preco">
+                                <h2>R$ 250,00</h2>
+                            </div>
+                       </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+
+        <section class="parallax">
+            <div class="overlay-parallax"></div>
+            <div class="center">
+                <h2>Sobre a nossa <span style="color: #2692ff">empresa</span></h2>
+            
+                <p>Sed id erat ultrices, rutrum enim scelerisque, euismod nunc. Praesent et tempor eros. 
+                        Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                         Pellentesque eleifend odio ut efficitur facilisis. Quisque lacus lectus, elementum ut pellentesque quis, 
+                         semper eget enim. Nam suscipit tempus sapien. Sed nec justo massa.</p> 
+            </div>
+        </section>
+
+        <section class="final-section">
+            <div class="center">
+                <div class="w-50 contato">
+                    <h2><div class="border-efeito"></div>Entre em contato</h2>
+
+                    <form>
+                        <input type="text" name="nome" placeholder="Insira seu nome">
+                        <input type="email" name="email" placeholder="Insira seu e-mail">
+                        <textarea placeholder="Insira sua mensagem"></textarea>
+                        <input type="submit">
+                    </form>
+                </div>
+
+                <div class="w-50 time">
+                    <h2><div class="border-efeito"></div>Conheça nosso time!</h2>
+                    
+                   <div class="w-50 equipe-single">
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+
+                            <div class="info-usuario">
+                                <p style="font-weight: bold;">Guilherme Grillo</p>
+                                <p style="font-weight: italic;">CEO / Instrutor</p>
+                            </div>
+                        </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                         semper eget enim. Nam suscipit tempus sapien. Sed nec justo massa</p>
+                        </div>
+                   </div> 
+
+                   <div class="w-50 equipe-single">                
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+
+                            <div class="info-usuario">
+                                <p style="font-weight: bold;">Guilherme Grillo</p>
+                                <p style="font-style: italic;">CEO / Instrutor</p>
+                            </div>
+                        </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                         semper eget enim. Nam suscipit tempus sapien. Sed nec justo massa</p>
+                        </div>
+                   </div> 
+
+                   <div class="w-50 equipe-single">
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+
+                            <div class="info-usuario">
+                                <p style="font-weight: bold;">Guilherme Grillo</p>
+                                <p style="font-weight: italic;">CEO / Instrutor</p>
+                            </div>
+                        </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                         semper eget enim. Nam suscipit tempus sapien. Sed nec justo massa</p>
+                        </div>
+                   </div> 
+
+                   <div class="w-50 equipe-single">                
+                        <div class="topo-box-usuario">
+                            <div class="box-avatar"></div>
+
+                            <div class="info-usuario">
+                                <p style="font-weight: bold;">Guilherme Grillo</p>
+                                <p style="font-style: italic;">CEO / Instrutor</p>
+                            </div>
+                        </div>
+
+                        <div class="descricao-usuario-box">
+                            <p>Integer posuere sit amet nisi ac faucibus. Suspendisse mattis suscipit nisl, iaculis accumsan nisl vestibulum vel.
+                         semper eget enim. Nam suscipit tempus sapien. Sed nec justo massa</p>
+                        </div>
+                   </div> 
+
+                   <div class="clear"></div>
+                </div>
+
+                <div class="clear"></div>
+            </div>
+        </section>
+
+        <footer>
+            <p>Todos os direitos reservados ao projeto 06</p>
+        </footer>
+
+        <script src="http://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+        <script>
+            $(function () {
+                $('nav.mobile i').click(() => {
+                    if($('.mobile ul').is(':visible') == false) {
+                        console.log("foi");
+                        $('.mobile ul').fadeIn();
+                    } else {
+                        $('.mobile ul').fadeOut();
+                    }
+                })
+            })
+        </script>
+    </body>
+</html>
